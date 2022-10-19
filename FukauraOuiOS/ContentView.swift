9ea6810e3782d29a9d56557ce30eb0884cbf4f02
select766 <select766@outlook.jp>
2022-10-19 22:06:20 +0900

    ふかうら王が動く

--- FukauraOuiOS/ContentView.swift
+++ FukauraOuiOS/ContentView.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
+import YaneuraOuiOSSPM
+
+func stringToUnsafeMutableBufferPointer(_ s: String) -> UnsafeMutableBufferPointer<Int8> {
+    let count = s.utf8CString.count
+    let result: UnsafeMutableBufferPointer<Int8> = UnsafeMutableBufferPointer<Int8>.allocate(capacity: count)
+    _ = result.initialize(from: s.utf8CString)
+    return result
+}
 
 struct ContentView: View {
+    @State private var usiHost = "127.0.0.1"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("FukauraOu iOS")
+            Text("将棋所の設定でDNN_Model1を空にする必要あり")
+            HStack {
+                Text("USI Host IP:")
+                TextField("", text: $usiHost)
+            }.padding()
+            Button(action: {
+                let host_p = stringToUnsafeMutableBufferPointer(usiHost)
+                print(DlShogiResnet.urlOfModelInThisBundle.absoluteString)
+                let model_url_p = stringToUnsafeMutableBufferPointer(DlShogiResnet.urlOfModelInThisBundle.absoluteString)
+                print("yaneuraou_ios_main", YaneuraOuiOSSPM.yaneuraou_ios_main(host_p.baseAddress!, 8090, model_url_p.baseAddress!))
+            }) {
+                Text("Run")
+            }
         }
         .padding()
     }
