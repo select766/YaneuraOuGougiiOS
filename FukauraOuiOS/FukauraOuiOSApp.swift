f72b555ff42cf39434157d952e7b71e014e2b144
select766 <select766@outlook.jp>
2022-10-19 20:43:08 +0900

    Initial Commit

---
+++ FukauraOuiOS/FukauraOuiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FukauraOuiOSApp.swift
+//  FukauraOuiOS
+//
+//  Created by Masatoshi Hidaka on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct FukauraOuiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
