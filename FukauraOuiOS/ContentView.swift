803b4f3100245ddb331328a2f96527a03a7c0098
select766 <select766@outlook.jp>
2022-10-20 18:39:29 +0900

    接続先IPを覚える機能

--- FukauraOuiOS/ContentView.swift
+++ FukauraOuiOS/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import YaneuraOuiOSSPM
 
+let userDefaultsUSIServerIpAddressKey = "usiServerIpAddress"
+
 func stringToUnsafeMutableBufferPointer(_ s: String) -> UnsafeMutableBufferPointer<Int8> {
     let count = s.utf8CString.count
     let result: UnsafeMutableBufferPointer<Int8> = UnsafeMutableBufferPointer<Int8>.allocate(capacity: count)
@@ -16,20 +18,36 @@ func stringToUnsafeMutableBufferPointer(_ s: String) -> UnsafeMutableBufferPoint
 }
 
 struct ContentView: View {
-    @State private var usiHost = "127.0.0.1"
+    @State private var usiHost: String = UserDefaults.standard.string(forKey: userDefaultsUSIServerIpAddressKey) ?? "127.0.0.1"
+    @State private var connectionStatus = "接続先を設定してRunをタップ"
+    @State private var running = false
+
     var body: some View {
         VStack {
             Text("FukauraOu iOS")
             Text("将棋所の設定でDNN_Model1を空にする必要あり")
+            Text(connectionStatus)
             HStack {
                 Text("USI Host IP:")
                 TextField("", text: $usiHost)
             }.padding()
             Button(action: {
+                if self.running {
+                    self.connectionStatus = "再接続はできません。アプリを再起動してください。"
+                    return
+                }
+                self.running = true
+                UserDefaults.standard.set(usiHost, forKey: userDefaultsUSIServerIpAddressKey)
                 let host_p = stringToUnsafeMutableBufferPointer(usiHost)
                 print(DlShogiResnet.urlOfModelInThisBundle.absoluteString)
                 let model_url_p = stringToUnsafeMutableBufferPointer(DlShogiResnet.urlOfModelInThisBundle.absoluteString)
-                print("yaneuraou_ios_main", YaneuraOuiOSSPM.yaneuraou_ios_main(host_p.baseAddress!, 8090, model_url_p.baseAddress!))
+                let mainResult = YaneuraOuiOSSPM.yaneuraou_ios_main(host_p.baseAddress!, 8090, model_url_p.baseAddress!)
+                print("yaneuraou_ios_main", mainResult)
+                if mainResult == 0 {
+                    self.connectionStatus = "接続成功"
+                } else {
+                    self.connectionStatus = "接続失敗"
+                }
             }) {
                 Text("Run")
             }
